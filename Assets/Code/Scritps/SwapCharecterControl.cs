3ee09df95112fe7a3ed6a223011bf55b666c6355
IceZK <ice.cm1@gmail.com>
2023-01-17 17:42:23 +0700

    camera change when switch character

--- Assets/Code/Scritps/SwapCharecterControl.cs
+++ Assets/Code/Scritps/SwapCharecterControl.cs
@@ -6,14 +6,23 @@ namespace IceDEV
 {
     public class SwapCharecterControl : MonoBehaviour
     {
+        [Header("Player")]
         public GameObject player1;
         public GameObject player2;
         private GameObject activePlayer;
+
+        [Header("Player")]
+        public GameObject cam1;
+        public GameObject cam2;
+        private GameObject activeCam;
 
         void Start()
         {
             activePlayer = player1;
             player2.GetComponent<CharacterController>().enabled = false;
+
+            activeCam = cam1;
+            cam2.active = false;
         }
 
         void Update()
@@ -21,6 +30,7 @@ namespace IceDEV
             if (Input.GetKeyDown(KeyCode.Tab))
             {
                 SwitchCharacter();
+                SwitchCamera();
             }
         }
 
@@ -37,6 +47,22 @@ namespace IceDEV
                 activePlayer = player1;
                 player1.GetComponent<CharacterController>().enabled = true;
                 player2.GetComponent<CharacterController>().enabled = false;
+            }
+        }
+
+        void SwitchCamera()
+        {
+            if (activeCam == cam1)
+            {
+                activeCam = cam2;
+                cam1.active = false;
+                cam2.active = true;
+            }
+            else
+            {
+                activeCam = cam1;
+                cam1.active = true;
+                cam2.active = false;
             }
         }
     }
