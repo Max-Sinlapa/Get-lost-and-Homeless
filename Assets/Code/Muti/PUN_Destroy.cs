24a7f0491d294d155516a976c2e094225b2ca7bc
IceZK <ice.cm1@gmail.com>
2023-03-09 14:54:43 +0700

    Add muti script for Lab

--- Assets/Code/Muti/PUN_Destroy.cs
+++ Assets/Code/Muti/PUN_Destroy.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
-public class PUN_Destroy : MonoBehaviourPun
+
+public class Destroy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
